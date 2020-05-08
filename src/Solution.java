80e85ee66e0966d77e2df4db9da7c15270770f64
Aditya Singh Chauhan <aditya.chuahan_cs18@gla.ac.in>
2020-05-08 22:09:07 +0530

    Initial Commit

--- src/Solution.java
+++ src/Solution.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.lang.Math;
 public class Solution
 {
-
     public static void main(String[] args)
     {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
