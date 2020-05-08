543bfb29986099df9502a24e452cf4c541eb8f76
Aditya Singh Chauhan <aditya.chuahan_cs18@gla.ac.in>
2020-05-08 21:59:47 +0530

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        long n = (long)Math.pow(a,b);
+        int x = 0;
+        while (n > 0 || x > 9)
+        {
+            if (n == 0) {
+                n = x;
+                x = 0;
+            }
+
+            x += n % 10;
+            n /= 10;
+        }
+        System.out.println(x);
+
+    }
+}
